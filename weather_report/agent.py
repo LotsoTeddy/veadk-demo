79cfdd21bdb6d0a345dd197b26047de0df0c7d62
wuqingfu.528 <wuqingfu.528@bytedance.com>
2025-09-05 16:41:20 +0800

    change

--- weather_report/agent.py
+++ weather_report/agent.py
@@ -24,25 +24,25 @@ from veadk.tools.demo_tools import get_city_weather
 # )
 
 
-def counting(a: int, b: int) -> int:
-    return a + b
-
-agent: Agent = Agent(
-    name="counting_agent",
-    # description="A reporter for weather updates",
-    instruction="一旦用户问你数学加法，必须调用 `counting` 工具计算.",
-    tools=[counting],
-)
-
-# def news(query: str) -> int:
-#     return "国铁集团对儿童、残疾军人、伤残人民警察、残疾消防救援人员等旅客购票优惠措施进一步优化，动车组列车优惠（待）票价计算基础由公布票价改为执行票价，优惠下限为公布票价的4折，同时优化调整动车组列车儿童票相应席别的优惠幅度。优惠车票9月6日开始发售。"
+# def counting(a: int, b: int) -> int:
+#     return a + b
 
 # agent: Agent = Agent(
-#     name="news_agent",
+#     name="counting_agent",
 #     # description="A reporter for weather updates",
-#     instruction="一旦用户问你今日新闻，必须调用 `news` 工具查询.",
-#     tools=[news],
+#     instruction="一旦用户问你数学加法，必须调用 `counting` 工具计算.",
+#     tools=[counting],
 # )
+
+def news(query: str) -> int:
+    return "国铁集团对儿童、残疾军人、伤残人民警察、残疾消防救援人员等旅客购票优惠措施进一步优化，动车组列车优惠（待）票价计算基础由公布票价改为执行票价，优惠下限为公布票价的4折，同时优化调整动车组列车儿童票相应席别的优惠幅度。优惠车票9月6日开始发售。"
+
+agent: Agent = Agent(
+    name="news_agent",
+    # description="A reporter for weather updates",
+    instruction="一旦用户问你今日新闻，必须调用 `news` 工具查询.",
+    tools=[news],
+)
 
 # required from Google ADK Web
 root_agent = agent
